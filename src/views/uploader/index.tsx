69670cf567e0dda86e00da58e865e6fe4bada2f4
Vikrant Ghodvinde <ghodvinde227@gmail.com>
2023-12-29 13:31:31 +0530

    updated

--- src/views/uploader/index.tsx
+++ src/views/uploader/index.tsx
@@ -9,7 +9,7 @@ export const UploaderView: FC = ({}) => {
       <div className="max-w-screen-sm mx-auto">
         <div className="text-center mb-14">
           <h2 className="text-4xl font-bold mb-5 bg-primary-gradient bg-clip-text text-transparent">
-            Update Metadata
+            Upload Metadata
           </h2>
           <p className="text-gray-400">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla
