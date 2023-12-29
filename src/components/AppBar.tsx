dd021ee28ed6e024aa016c390514c87378934f0b
Vikrant Ghodvinde <ghodvinde227@gmail.com>
2023-12-29 13:22:21 +0530

    updated

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -36,46 +36,6 @@ export const AppBar: FC = (props) => {
           </span>
         </a>
         <div className="flex items-center gap-4 lg:order-2 space-x-3 lg:space-x-0 rtl:space-x-reverse">
-          <div className="dropdown">
-            <div tabIndex={0} className="p-0">
-              <svg
-                className="w-6 h-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="shadow menu top-12 dropdown-content bg-dark rounded sm:w-52"
-            >
-              <li>
-                <label className="cursor-pointer label">
-                  <a>Auto Connect</a>
-                  <input
-                    type="checkbox"
-                    checked={autoConnect}
-                    onChange={(e) => setAutoConnect(e.target.checked)}
-                    className="toggle toggle-success"
-                  />
-                </label>
-              </li>
-            </ul>
-          </div>
           <WalletMultiButton className="relative text-black bg-primary-gradient text-sm px-6 py-2 text-center rounded z-10 before:absolute before:w-full before:h-full before:bg-primary-gradient-reversed before:top-0 before:left-0 before:-z-10 before:opacity-0 before:hover:opacity-100 before:transition-all before:delay-150 font-semibold capitalize overflow-hidden" />
           <button
             data-collapse-toggle="navbar-sticky"
