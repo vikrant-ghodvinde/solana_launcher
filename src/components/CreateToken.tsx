c91d1f5c78d3b6cd2cd6c90d7c48d4eb863cc7f2
Vikrant Ghodvinde <ghodvinde227@gmail.com>
2023-12-29 13:08:10 +0530

    updated

--- src/components/CreateToken.tsx
+++ src/components/CreateToken.tsx
@@ -238,6 +238,44 @@ export const CreateToken: FC = () => {
 
   return (
     <div className="grid grid-cols-2 gap-4">
+      <div className="relative col-span-2">
+        <div className="relative w-full flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+          <div className="space-y-1 text-center">
+            <svg
+              className="mx-auto h-12 w-12 text-gray-400"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 48 48"
+              aria-hidden="true"
+            >
+              <path
+                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <div className="flex text-sm text-gray-600">
+              <label
+                htmlFor="image-upload"
+                className="w-full h-full cursor-pointe rounded-md font-medium text-primary-color hover:text-secondary-color focus-within:outline-none"
+              >
+                <span>Upload an Image</span>
+                <input
+                  id="image-upload"
+                  name="image-upload"
+                  type="file"
+                  className="sr-only absolute w-full h-full top-0 left-0"
+                  // onChange={handleMetadataChange}
+                />
+              </label>
+            </div>
+            {/* {selectedFile && (
+              <p className="text-sm text-gray-500">{selectedFile}</p>
+            )} */}
+          </div>
+        </div>
+      </div>
       <div className="relative col-span-2 sm:col-span-1">
         <label htmlFor="" className="block text-white mb-2">
           Token name
